184d0c95fd8ced608d5221990d1410d3bdd37185
JAICHANGPARK <aristojeff@gmail.com>
2025-08-29 17:09:48 +0900

    Update furniture_store_home_page.dart

--- lib/ep2351_furniture_store_app/ui/furniture_store_home_page.dart
+++ lib/ep2351_furniture_store_app/ui/furniture_store_home_page.dart
@@ -14,7 +14,7 @@ class _FurnitureStoreHomePageState extends State<FurnitureStoreHomePage> {
     return Scaffold(
       body: Stack(
         children: [
-          
+
         ],
       ),
     );
