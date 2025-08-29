3615e7e6c83070d6d43b61fafd0edad129222c7e
JAICHANGPARK <aristojeff@gmail.com>
2025-08-29 17:06:36 +0900

    Update ep2351_main.dart

--- lib/ep2351_furniture_store_app/ep2351_main.dart
+++ lib/ep2351_furniture_store_app/ep2351_main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_47/ep2351_furniture_store_app/furniture_store_app.dart';
 
 
 void main() {
-  
+ runApp(FurnitureStoreApp());
 }
