d1286a00163c5d492c7650091a498af43c0f437e
JAICHANGPARK <aristojeff@gmail.com>
2025-08-29 17:06:03 +0900

    Update furniture_store_app.dart

---
+++ lib/ep2351_furniture_store_app/furniture_store_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class FurnitureStoreApp extends StatelessWidget {
+  const FurnitureStoreApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
