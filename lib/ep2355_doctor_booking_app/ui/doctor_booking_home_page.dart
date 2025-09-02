dd1da555b0bd69934181d6b4144ad0cc11f98091
JAICHANGPARK <aristojeff@gmail.com>
2025-09-02 18:34:24 +0900

    Update doctor_booking_home_page.dart

--- lib/ep2355_doctor_booking_app/ui/doctor_booking_home_page.dart
+++ lib/ep2355_doctor_booking_app/ui/doctor_booking_home_page.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class DoctorBookingHomePage extends StatefulWidget {
+  const DoctorBookingHomePage({super.key});
+
+  @override
+  State<DoctorBookingHomePage> createState() => _DoctorBookingHomePageState();
+}
+
+class _DoctorBookingHomePageState extends State<DoctorBookingHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
