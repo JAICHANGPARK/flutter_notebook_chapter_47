8dfaa4a13ae406b14f0427a194c6a223f6bbf261
JAICHANGPARK <aristojeff@gmail.com>
2025-09-02 18:35:03 +0900

    Update ep2355_main.dart

--- lib/ep2355_doctor_booking_app/ep2355_main.dart
+++ lib/ep2355_doctor_booking_app/ep2355_main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_47/ep2355_doctor_booking_app/doctor_booking_app.dart';
 
 void main() {
-  
+  runApp(DoctorBookingApp());
 }
