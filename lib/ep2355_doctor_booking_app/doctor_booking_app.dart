4bd6a7984b995b99cc45b6f99f27a4130e45c3bb
JAICHANGPARK <aristojeff@gmail.com>
2025-09-02 18:33:45 +0900

    Update doctor_booking_app.dart

---
+++ lib/ep2355_doctor_booking_app/doctor_booking_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class DoctorBookingApp extends StatelessWidget {
+  const DoctorBookingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
