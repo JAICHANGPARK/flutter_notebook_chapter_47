db18aac03109a2b9ec4069d096f188e7f9847ac3
JAICHANGPARK <aristojeff@gmail.com>
2025-09-07 10:46:40 +0900

    Update social_media_app.dart

--- lib/ep2360_social_media_app/social_media_app.dart
+++ lib/ep2360_social_media_app/social_media_app.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'ui/social_media_home_page.dart';
+
 class SocialMediaApp extends StatelessWidget {
   const SocialMediaApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      home: SocialMediaHomePage(),
     );
   }
 }
