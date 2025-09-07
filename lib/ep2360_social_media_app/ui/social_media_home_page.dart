040bc03f42b86f9828927dbd9bab7682b76aed74
JAICHANGPARK <aristojeff@gmail.com>
2025-09-07 10:46:29 +0900

    Update social_media_home_page.dart

---
+++ lib/ep2360_social_media_app/ui/social_media_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SocialMediaHomePage extends StatefulWidget {
+  const SocialMediaHomePage({super.key});
+
+  @override
+  State<SocialMediaHomePage> createState() => _SocialMediaHomePageState();
+}
+
+class _SocialMediaHomePageState extends State<SocialMediaHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
