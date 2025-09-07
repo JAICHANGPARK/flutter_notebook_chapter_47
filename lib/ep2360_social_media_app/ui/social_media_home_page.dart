7ed8d03efb2ff349a08ab8541817ce08de8cbe4d
JAICHANGPARK <aristojeff@gmail.com>
2025-09-07 10:51:43 +0900

    Update social_media_home_page.dart

--- lib/ep2360_social_media_app/ui/social_media_home_page.dart
+++ lib/ep2360_social_media_app/ui/social_media_home_page.dart
@@ -11,27 +11,24 @@ class _SocialMediaHomePageState extends State<SocialMediaHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Column(children: [
-        Row(
+      body: SafeArea(
+        child: Column(
           children: [
-            CircleAvatar()
+            Row(children: [CircleAvatar()]),
+            SizedBox(height: 100, child: Placeholder()),
+            Row(children: []),
+            SizedBox(height: 200, child: Placeholder()),
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  return Container(height: 320,
+                  child: Placeholder(),);
+                },
+              ),
+            ),
           ],
         ),
-        SizedBox(
-          height: 100,
-          child: Placeholder(),
-        ),
-        Row(
-          children: [
-
-          ],
-        ),
-        SizedBox(
-          height: 200,
-          child: Placeholder(),
-        ),
-        Expanded(child: ListView.builder(itemBuilder: (context,index){},))
-      ])),
+      ),
       bottomNavigationBar: Container(
         height: 80,
         decoration: BoxDecoration(color: Colors.blue),
