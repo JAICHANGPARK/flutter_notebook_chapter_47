19ec02b5b06e59427dcd6480fd00abe6aac5c0df
JAICHANGPARK <aristojeff@gmail.com>
2025-09-08 17:24:33 +0900

    Update social_media_home_page.dart

--- lib/ep2360_social_media_app/ui/social_media_home_page.dart
+++ lib/ep2360_social_media_app/ui/social_media_home_page.dart
@@ -31,32 +31,36 @@ class _SocialMediaHomePageState extends State<SocialMediaHomePage> {
           ],
         ),
       ),
-      bottomNavigationBar: Container(
+      bottomNavigationBar: SizedBox(
         height: 100,
-        child: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(HugeIcons.strokeRoundedHome01),
-              label: "Home",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(HugeIcons.strokeRoundedHome01),
-              label: "Reels",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(HugeIcons.strokeRoundedHome01),
-              label: "Friends",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(HugeIcons.strokeRoundedHome01),
-              label: "Notifications",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(HugeIcons.strokeRoundedMenu01),
-              label: "Menu",
-            ),
-          ],
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(topLeft: Radius.circular(24)),
+          child: BottomNavigationBar(
+            
+            type: BottomNavigationBarType.fixed,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(HugeIcons.strokeRoundedHome01),
+                label: "Home",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(HugeIcons.strokeRoundedHome01),
+                label: "Reels",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(HugeIcons.strokeRoundedHome01),
+                label: "Friends",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(HugeIcons.strokeRoundedHome01),
+                label: "Notifications",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(HugeIcons.strokeRoundedMenu01),
+                label: "Menu",
+              ),
+            ],
+          ),
         ),
       ),
     );
