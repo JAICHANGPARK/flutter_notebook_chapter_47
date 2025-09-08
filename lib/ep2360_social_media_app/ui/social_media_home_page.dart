a37d18fc1e4ce7172bc992ba9c1447c0134d5f4a
JAICHANGPARK <aristojeff@gmail.com>
2025-09-08 17:22:52 +0900

    Update social_media_home_page.dart

--- lib/ep2360_social_media_app/ui/social_media_home_page.dart
+++ lib/ep2360_social_media_app/ui/social_media_home_page.dart
@@ -33,7 +33,26 @@ class _SocialMediaHomePageState extends State<SocialMediaHomePage> {
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: [
-          BottomNavigationBarItem(icon: Icon(HugeIcons.strokeRoundedHome01)),
+          BottomNavigationBarItem(
+            icon: Icon(HugeIcons.strokeRoundedHome01),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(HugeIcons.strokeRoundedHome01),
+            label: "Reels",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(HugeIcons.strokeRoundedHome01),
+            label: "Reels",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(HugeIcons.strokeRoundedHome01),
+            label: "Reels",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(HugeIcons.strokeRoundedHome01),
+            label: "Reels",
+          ),
         ],
       ),
     );
