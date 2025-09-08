26c8ea0099168bbf456dc004aeb7622708e594c4
JAICHANGPARK <aristojeff@gmail.com>
2025-09-08 17:38:46 +0900

    Update social_media_home_page.dart

--- lib/ep2360_social_media_app/ui/social_media_home_page.dart
+++ lib/ep2360_social_media_app/ui/social_media_home_page.dart
@@ -39,61 +39,70 @@ class _SocialMediaHomePageState extends State<SocialMediaHomePage> {
                 ],
               ),
             ),
-            SizedBox(
-              height: 80,
+            Container(
+              padding: EdgeInsets.only(
+                left: 16
+              ),
+              height: 100,
               child: ListView(
                 scrollDirection: Axis.horizontal,
                 children: [
                   Container(
-                    height: 80,
+                    height: 100,
                     width: 80,
                     child: Column(
+                      spacing: 12,
                       children: [
-                        Stack(
-                          children: [
-                            Positioned.fill(
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  gradient: LinearGradient(
-                                    begin: Alignment.bottomLeft,
-                                    end: Alignment.topRight,
-                                    colors: [
-                                      Colors.yellow,
-                                      Colors.orange,
-                                      Colors.red,
-                                      Colors.red,
-                                    ],
-                                  ),
-                                ),
-                                padding: EdgeInsets.all(3),
+                        Expanded(
+                          child: Stack(
+                            children: [
+                              Positioned.fill(
                                 child: Container(
                                   decoration: BoxDecoration(
                                     shape: BoxShape.circle,
-                                    border: Border.all(
-                                      color: Colors.black,
-                                      width: 1.5,
+                                    gradient: LinearGradient(
+                                      begin: Alignment.bottomLeft,
+                                      end: Alignment.topRight,
+                                      colors: [
+                                        Colors.yellow,
+                                        Colors.orange,
+                                        Colors.red,
+                                        Colors.red,
+                                      ],
                                     ),
-                                    color: Colors.grey,
+                                  ),
+                                  padding: EdgeInsets.all(3),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      border: Border.all(
+                                        color: Colors.black,
+                                        width: 1.5,
+                                      ),
+                                      color: Colors.grey,
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                            Positioned(
-                              bottom: 0,
-                              right: 0,
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Colors.blue,
-                                  border: Border.all(color: Colors.white,
-                                  width: 1.5),
+                              Positioned(
+                                bottom: 0,
+                                right: 0,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Colors.blue,
+                                    border: Border.all(
+                                      color: Colors.white,
+                                      width: 1.5,
+                                    ),
+                                  ),
+                                  child: Icon(Icons.add, size: 20),
                                 ),
-                                child: Icon(Icons.add, size: 20),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
+                        Text("Dream", style: TextStyle(color: Colors.grey)),
                       ],
                     ),
                   ),
