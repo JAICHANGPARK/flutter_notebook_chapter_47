8f1cc710a2236155ac77c4ce9f8495ad8d78d594
JAICHANGPARK <aristojeff@gmail.com>
2025-09-11 16:55:01 +0900

    Update e_learning_app.dart

--- lib/ep2364_e_learning_app/e_learning_app.dart
+++ lib/ep2364_e_learning_app/e_learning_app.dart
@@ -1,13 +1,15 @@
 
 import 'package:flutter/material.dart';
 
+import 'ui/e_learning_home_page.dart';
+
 class ELearningApp extends StatelessWidget {
   const ELearningApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      home: ELearningHomePage(),
     );
   }
 }
