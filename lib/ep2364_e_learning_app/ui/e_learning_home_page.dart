7b2dba20769870d695c8fb767fc96bbdd81be6bf
JAICHANGPARK <aristojeff@gmail.com>
2025-09-11 16:56:01 +0900

    Update e_learning_home_page.dart

--- lib/ep2364_e_learning_app/ui/e_learning_home_page.dart
+++ lib/ep2364_e_learning_app/ui/e_learning_home_page.dart
@@ -11,8 +11,23 @@ class _ELearningHomePageState extends State<ELearningHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(),
+      body: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.green
+            ),
+            padding: EdgeInsets.fromLTRB(16, 42, 16, 16),
+            child: Column(
+              children: [
+                
+              ],
+            ),
+          )
+        ],
+      ),
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: "Home"),
           BottomNavigationBarItem(
