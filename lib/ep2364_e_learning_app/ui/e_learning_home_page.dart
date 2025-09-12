f4deae181e98f052af6600503350edd81aa7bfcf
JAICHANGPARK <aristojeff@gmail.com>
2025-09-13 08:30:00 +0900

    Update e_learning_home_page.dart

--- lib/ep2364_e_learning_app/ui/e_learning_home_page.dart
+++ lib/ep2364_e_learning_app/ui/e_learning_home_page.dart
@@ -62,14 +62,14 @@ class _ELearningHomePageState extends State<ELearningHomePage> {
                   ],
                 ),
                 Container(
-                  decoration: BoxDecoration(),
+                  decoration: ShapeDecoration(shape: StadiumBorder()),
                   child: Row(
                     children: [
                       Icon(Icons.search),
                       Expanded(child: TextField()),
                       CircleAvatar(
                         child: Icon(Icons.tune),
-                        backgroundColor: Colors.green[100],
+                        backgroundColor: Colors.green[50],
                         foregroundColor: Colors.green,
                       ),
                     ],
