2e299e95b81a08da3847f5ed45565ded3c4c1a36
JAICHANGPARK <aristojeff@gmail.com>
2025-09-13 08:14:43 +0900

    Update social_media_home_page.dart

--- lib/ep2360_social_media_app/ui/social_media_home_page.dart
+++ lib/ep2360_social_media_app/ui/social_media_home_page.dart
@@ -107,66 +107,68 @@ class _SocialMediaHomePageState extends State<SocialMediaHomePage> {
                   ),
                   ...List.generate(
                     8,
-                    (idx) => Container(
-                      height: 100,
-                      width: 80,
-                      margin: EdgeInsets.only(right: 8),
-                      child: Column(
-                        spacing: 6,
-                        children: [
-                          Expanded(
-                            child: Stack(
-                              children: [
-                                Positioned.fill(
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      shape: BoxShape.circle,
-                                      gradient: LinearGradient(
-                                        begin: Alignment.bottomLeft,
-                                        end: Alignment.topRight,
-                                        colors: [
-                                          Colors.yellow,
-                                          Colors.orange,
-                                          Colors.red,
-                                          Colors.red,
-                                        ],
-                                      ),
-                                    ),
-                                    padding: EdgeInsets.all(3),
-                                    child: Container(
-                                      decoration: BoxDecoration(
-                                        shape: BoxShape.circle,
-                                        border: Border.all(
-                                          color: Colors.black,
-                                          width: 1.5,
+                        (idx) =>
+                        Container(
+                          height: 100,
+                          width: 80,
+                          margin: EdgeInsets.only(right: 8),
+                          child: Column(
+                            spacing: 6,
+                            children: [
+                              Expanded(
+                                child: Stack(
+                                  children: [
+                                    Positioned.fill(
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                          shape: BoxShape.circle,
+                                          gradient: LinearGradient(
+                                            begin: Alignment.bottomLeft,
+                                            end: Alignment.topRight,
+                                            colors: [
+                                              Colors.yellow,
+                                              Colors.orange,
+                                              Colors.red,
+                                              Colors.red,
+                                            ],
+                                          ),
                                         ),
-                                        color: Colors.grey,
+                                        padding: EdgeInsets.all(3),
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            border: Border.all(
+                                              color: Colors.black,
+                                              width: 1.5,
+                                            ),
+                                            color: Colors.grey,
+                                          ),
+                                        ),
                                       ),
                                     ),
-                                  ),
+                                    // Positioned(
+                                    //   bottom: 0,
+                                    //   right: 0,
+                                    //   child: Container(
+                                    //     decoration: BoxDecoration(
+                                    //       shape: BoxShape.circle,
+                                    //       color: Colors.blue,
+                                    //       border: Border.all(
+                                    //         color: Colors.white,
+                                    //         width: 1.5,
+                                    //       ),
+                                    //     ),
+                                    //     child: Icon(Icons.add, size: 20),
+                                    //   ),
+                                    // ),
+                                  ],
                                 ),
-                                // Positioned(
-                                //   bottom: 0,
-                                //   right: 0,
-                                //   child: Container(
-                                //     decoration: BoxDecoration(
-                                //       shape: BoxShape.circle,
-                                //       color: Colors.blue,
-                                //       border: Border.all(
-                                //         color: Colors.white,
-                                //         width: 1.5,
-                                //       ),
-                                //     ),
-                                //     child: Icon(Icons.add, size: 20),
-                                //   ),
-                                // ),
-                              ],
-                            ),
+                              ),
+                              Text("Dream",
+                                  style: TextStyle(color: Colors.grey)),
+                            ],
                           ),
-                          Text("Dream", style: TextStyle(color: Colors.grey)),
-                        ],
-                      ),
-                    ),
+                        ),
                   ),
                 ],
               ),
@@ -256,7 +258,7 @@ class _SocialMediaHomePageState extends State<SocialMediaHomePage> {
                                       child: Column(
                                         spacing: 4,
                                         crossAxisAlignment:
-                                            CrossAxisAlignment.start,
+                                        CrossAxisAlignment.start,
                                         children: [
                                           Text(
                                             "Dream Walker",
@@ -353,7 +355,8 @@ class _SocialMediaHomePageState extends State<SocialMediaHomePage> {
                 label: "Home",
               ),
               BottomNavigationBarItem(
-                icon: Icon(HugeIcons.strokeRoundedCamera01),
+                icon: HugeIcon(
+                  icon: HugeIcons.strokeRoundedCamera01, color: Colors.grey,),
                 label: "Reels",
               ),
               BottomNavigationBarItem(
