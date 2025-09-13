938dc5a5e069a1408685d33d0a1ef0f3697fd339
JAICHANGPARK <aristojeff@gmail.com>
2025-09-14 07:45:38 +0900

    Update e_learning_home_page.dart

--- lib/ep2364_e_learning_app/ui/e_learning_home_page.dart
+++ lib/ep2364_e_learning_app/ui/e_learning_home_page.dart
@@ -165,7 +165,7 @@ class _ELearningHomePageState extends State<ELearningHomePage> {
                           margin: EdgeInsets.only(right: 16),
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(16),
-                            border: Border.all(color: Colors.grey),
+                            border: Border.all(color: Colors.grey[400]!),
                           ),
                           child: Column(
                             children: [
