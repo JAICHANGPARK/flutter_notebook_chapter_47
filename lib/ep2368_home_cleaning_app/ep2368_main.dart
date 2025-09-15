a3eb4f35cca3c2267b39bc8091599d81f6867f78
JAICHANGPARK <aristojeff@gmail.com>
2025-09-15 16:59:32 +0900

    Update ep2368_main.dart

---
+++ lib/ep2368_home_cleaning_app/ep2368_main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_notebook_chapter_47/ep2368_home_cleaning_app/home_cleaning_service_app.dart';
+
+void main() {
+  runApp(HomeCleaningServiceApp());
+}
