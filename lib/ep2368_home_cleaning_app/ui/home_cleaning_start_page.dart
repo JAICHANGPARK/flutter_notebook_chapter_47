a1dbab1b35105e65f3db95badd7f32b23c02ca3f
JAICHANGPARK <aristojeff@gmail.com>
2025-09-15 17:01:06 +0900

    Update home_cleaning_start_page.dart

--- lib/ep2368_home_cleaning_app/ui/home_cleaning_start_page.dart
+++ lib/ep2368_home_cleaning_app/ui/home_cleaning_start_page.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 
-
 class HomeCleaningStartPage extends StatelessWidget {
   const HomeCleaningStartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Stack(
+        children: [
+          Image.network(
+            "https://cdn.pixabay.com/photo/2025/06/16/12/52/cleaning-services-9663247_1280.jpg",
+          ),
+        ],
+      ),
     );
   }
 }
