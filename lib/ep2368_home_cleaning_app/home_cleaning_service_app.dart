df3c2412a77770576e15506da24e7d56c1e9a186
JAICHANGPARK <aristojeff@gmail.com>
2025-09-15 16:58:07 +0900

    Update home_cleaning_service_app.dart

--- lib/ep2368_home_cleaning_app/home_cleaning_service_app.dart
+++ lib/ep2368_home_cleaning_app/home_cleaning_service_app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'ui/home_cleaning_start_page.dart';
+
 
 class HomeCleaningServiceApp extends StatelessWidget {
   const HomeCleaningServiceApp({super.key});
@@ -7,7 +9,7 @@ class HomeCleaningServiceApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      home: HomeCleaningStartPage(),
     );
   }
 }
