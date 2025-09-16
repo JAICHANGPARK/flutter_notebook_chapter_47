1f22917632cd3b1ba68d4867a023f60429cdb8c0
JAICHANGPARK <aristojeff@gmail.com>
2025-09-16 19:11:32 +0900

    Update home_cleaning_home_page.dart

--- lib/ep2368_home_cleaning_app/ui/home_cleaning_home_page.dart
+++ lib/ep2368_home_cleaning_app/ui/home_cleaning_home_page.dart
@@ -16,80 +16,102 @@ class _HomeCleaningHomePageState extends State<HomeCleaningHomePage> {
       backgroundColor: Color.fromRGBO(244, 246, 248, 1),
       body: Stack(
         children: [
-          SafeArea(
-            bottom: false,
-            child: Column(
-              spacing: 16,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 16,
-                    vertical: 8,
-                  ),
-                  child: Column(
-                    spacing: 24,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          CircleAvatar(
-                            radius: 24,
-                            backgroundColor: Colors.grey,
-                          ),
-                          CircleAvatar(
-                            radius: 25,
-                            backgroundColor: Colors.white,
-                            child: Badge(
-                              child: HugeIcon(
-                                icon: HugeIcons.strokeRoundedNotification01,
-                                color: Colors.black,
+          Positioned.fill(
+            child: SafeArea(
+              bottom: false,
+              child: Column(
+                spacing: 16,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 8,
+                    ),
+                    child: Column(
+                      spacing: 24,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            CircleAvatar(
+                              radius: 24,
+                              backgroundColor: Colors.grey,
+                            ),
+                            CircleAvatar(
+                              radius: 25,
+                              backgroundColor: Colors.white,
+                              child: Badge(
+                                child: HugeIcon(
+                                  icon: HugeIcons.strokeRoundedNotification01,
+                                  color: Colors.black,
+                                ),
                               ),
                             ),
-                          ),
-                        ],
-                      ),
-                      Container(
-                        decoration: ShapeDecoration(
-                          shape: StadiumBorder(),
-                          color: Colors.white,
+                          ],
                         ),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            hintText: "Search",
-                            border: InputBorder.none,
-                            icon: Icon(Icons.search),
+                        Container(
+                          decoration: ShapeDecoration(
+                            shape: StadiumBorder(),
+                            color: Colors.white,
+                          ),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: "Search",
+                              border: InputBorder.none,
+                              icon: Icon(Icons.search),
+                            ),
                           ),
                         ),
-                      ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Categories"),
+                      TextButton(onPressed: () {}, child: Text("See All")),
                     ],
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Categories"),
-                    TextButton(onPressed: () {}, child: Text("See All")),
-                  ],
-                ),
-                Container(height: 100, child: Placeholder()),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Cleaning Services"),
-                    TextButton(onPressed: () {}, child: Text("See All")),
-                  ],
-                ),
-                Container(height: 180, child: Placeholder()),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Home Repairs"),
-                    TextButton(onPressed: () {}, child: Text("See All")),
-                  ],
-                ),
-                Container(height: 180, child: Placeholder()),
-              ],
+                  Container(height: 100, child: Placeholder()),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Cleaning Services"),
+                      TextButton(onPressed: () {}, child: Text("See All")),
+                    ],
+                  ),
+                  Container(height: 180, child: Placeholder()),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Home Repairs"),
+                      TextButton(onPressed: () {}, child: Text("See All")),
+                    ],
+                  ),
+                  Container(height: 180, child: Placeholder()),
+                ],
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  CircleAvatar(),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.chat_outlined)),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.bookmark_border),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.person_2_outlined),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
