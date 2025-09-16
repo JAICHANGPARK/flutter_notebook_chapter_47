5170cffe23793efeea92f740c70c4f177969c33d
JAICHANGPARK <aristojeff@gmail.com>
2025-09-16 19:10:26 +0900

    Update home_cleaning_home_page.dart

--- lib/ep2368_home_cleaning_app/ui/home_cleaning_home_page.dart
+++ lib/ep2368_home_cleaning_app/ui/home_cleaning_home_page.dart
@@ -14,84 +14,89 @@ class _HomeCleaningHomePageState extends State<HomeCleaningHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(244, 246, 248, 1),
-      body: SafeArea(
-        bottom: false,
-        child: Column(
-          spacing: 16,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: Column(
-                spacing: 24,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Stack(
+        
+        children: [
+          SafeArea(
+            bottom: false,
+            child: Column(
+              spacing: 16,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: Column(
+                    spacing: 24,
                     children: [
-                      CircleAvatar(radius: 24, backgroundColor: Colors.grey),
-                      CircleAvatar(
-                        radius: 25,
-                        backgroundColor: Colors.white,
-                        child: Badge(
-                          child: HugeIcon(
-                            icon: HugeIcons.strokeRoundedNotification01,
-                            color: Colors.black,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          CircleAvatar(radius: 24, backgroundColor: Colors.grey),
+                          CircleAvatar(
+                            radius: 25,
+                            backgroundColor: Colors.white,
+                            child: Badge(
+                              child: HugeIcon(
+                                icon: HugeIcons.strokeRoundedNotification01,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        decoration: ShapeDecoration(
+                          shape: StadiumBorder(),
+                          color: Colors.white,
+                        ),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: "Search",
+                            border: InputBorder.none,
+                            icon: Icon(Icons.search),
                           ),
                         ),
                       ),
                     ],
                   ),
-                  Container(
-                    decoration: ShapeDecoration(
-                      shape: StadiumBorder(),
-                      color: Colors.white,
-                    ),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        hintText: "Search",
-                        border: InputBorder.none,
-                        icon: Icon(Icons.search),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("Categories"),
-                TextButton(onPressed: () {}, child: Text("See All")),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Categories"),
+                    TextButton(onPressed: () {}, child: Text("See All")),
+                  ],
+                ),
+                Container(
+                  height: 100,
+                  child: Placeholder(),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Cleaning Services"),
+                    TextButton(onPressed: () {}, child: Text("See All")),
+                  ],
+                ),
+                Container(
+                  height: 180,
+                  child: Placeholder(),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Home Repairs"),
+                    TextButton(onPressed: () {}, child: Text("See All")),
+                  ],
+                ),
+                Container(
+                  height: 180,
+                  child: Placeholder(),
+                )
               ],
             ),
-            Container(
-              height: 100,
-              child: Placeholder(),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("Cleaning Services"),
-                TextButton(onPressed: () {}, child: Text("See All")),
-              ],
-            ),
-            Container(
-              height: 180,
-              child: Placeholder(),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("Cleaning Services"),
-                TextButton(onPressed: () {}, child: Text("See All")),
-              ],
-            ),
-            Container(
-              height: 180,
-              child: Placeholder(),
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
