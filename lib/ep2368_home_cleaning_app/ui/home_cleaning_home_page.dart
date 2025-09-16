73eeb350468be0fce97b2548bee557f633b6790d
JAICHANGPARK <aristojeff@gmail.com>
2025-09-16 18:54:14 +0900

    Create home_cleaning_home_page.dart

---
+++ lib/ep2368_home_cleaning_app/ui/home_cleaning_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class HomeCleaningHomePage extends StatefulWidget {
+  const HomeCleaningHomePage({super.key});
+
+  @override
+  State<HomeCleaningHomePage> createState() => _HomeCleaningHomePageState();
+}
+
+class _HomeCleaningHomePageState extends State<HomeCleaningHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
