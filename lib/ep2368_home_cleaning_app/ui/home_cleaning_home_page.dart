27a10258ab93b2798189cbab77bf6f11627676df
JAICHANGPARK <aristojeff@gmail.com>
2025-09-16 19:05:43 +0900

    Update home_cleaning_home_page.dart

--- lib/ep2368_home_cleaning_app/ui/home_cleaning_home_page.dart
+++ lib/ep2368_home_cleaning_app/ui/home_cleaning_home_page.dart
@@ -13,37 +13,40 @@ class _HomeCleaningHomePageState extends State<HomeCleaningHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(244, 246, 248, 1),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    CircleAvatar(),
-                    CircleAvatar(
-                      child: Badge(
-                        child: HugeIcon(
-                          icon: HugeIcons.strokeRoundedNotification01,
-                          color: Colors.black,
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      CircleAvatar(radius: 24),
+                      CircleAvatar(
+                        radius: 25,
+                        child: Badge(
+                          child: HugeIcon(
+                            icon: HugeIcons.strokeRoundedNotification01,
+                            color: Colors.black,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                Container(
-                  decoration: ShapeDecoration(
-                    shape: StadiumBorder(),
-                    color: Colors.white,
+                    ],
                   ),
-                ),
-              ],
+                  Container(
+                    decoration: ShapeDecoration(
+                      shape: StadiumBorder(),
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          )
-
-        ],
+          ],
+        ),
       ),
     );
   }
