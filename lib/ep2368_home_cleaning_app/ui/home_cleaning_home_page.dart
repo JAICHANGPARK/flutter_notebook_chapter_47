38666d5cb4cd8a3ea2ea38f4af44451fa72fcf07
JAICHANGPARK <aristojeff@gmail.com>
2025-09-17 18:34:21 +0900

    Update home_cleaning_home_page.dart

--- lib/ep2368_home_cleaning_app/ui/home_cleaning_home_page.dart
+++ lib/ep2368_home_cleaning_app/ui/home_cleaning_home_page.dart
@@ -67,192 +67,198 @@ class _HomeCleaningHomePageState extends State<HomeCleaningHomePage> {
                       ],
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Categories",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {},
-                          style: TextButton.styleFrom(
-                            foregroundColor: Colors.purple,
-                            textStyle: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
+                  Expanded(child: SingleChildScrollView(child: Column(
+                    spacing: 12,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "Categories",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
                             ),
-                          ),
-                          child: Text("See All"),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    height: 100,
-                    padding: EdgeInsets.only(left: 16),
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-
-                      itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.only(right: 12),
-                          child: Column(
-                            spacing: 12,
-                            children: [
-                              CircleAvatar(radius: 32),
-                              Text(
-                                "Cleaning",
-                                style: TextStyle(
+                            TextButton(
+                              onPressed: () {},
+                              style: TextButton.styleFrom(
+                                foregroundColor: Colors.purple,
+                                textStyle: TextStyle(
+                                  fontSize: 16,
                                   fontWeight: FontWeight.bold,
-                                  color: Colors.grey,
                                 ),
                               ),
-                            ],
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Cleaning Services",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {},
-                          style: TextButton.styleFrom(
-                            foregroundColor: Colors.purple,
-                            textStyle: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
+                              child: Text("See All"),
                             ),
-                          ),
-                          child: Text("See All"),
+                          ],
                         ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    height: 200,
-                    padding: EdgeInsets.only(
-                      left: 16
-                    ),
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (context, index) {
-                        return Container(
-                          margin: EdgeInsets.only(right: 12),
-                          width: 150,
-                          decoration: ShapeDecoration(
-                            shape: RoundedSuperellipseBorder(
-                              borderRadius: BorderRadius.circular(24),
+                      ),
+                      Container(
+                        height: 100,
+                        padding: EdgeInsets.only(left: 16),
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding: const EdgeInsets.only(right: 12),
+                              child: Column(
+                                spacing: 12,
+                                children: [
+                                  CircleAvatar(radius: 32),
+                                  Text(
+                                    "Cleaning",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "Cleaning Services",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
                             ),
-                            color: Colors.white,
-                          ),
-                          padding: EdgeInsets.all(12),
-                          child: Column(
-                            spacing: 16,
-                            children: [
-                              Expanded(
-                                child: Container(
-                                  decoration: ShapeDecoration(
-                                    shape: RoundedSuperellipseBorder(
-                                      borderRadius: BorderRadius.only(
-                                        topRight: Radius.circular(24),
-                                        topLeft: Radius.circular(24),
+                            TextButton(
+                              onPressed: () {},
+                              style: TextButton.styleFrom(
+                                foregroundColor: Colors.purple,
+                                textStyle: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              child: Text("See All"),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 200,
+                        padding: EdgeInsets.only(
+                            left: 16
+                        ),
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              margin: EdgeInsets.only(right: 12),
+                              width: 150,
+                              decoration: ShapeDecoration(
+                                shape: RoundedSuperellipseBorder(
+                                  borderRadius: BorderRadius.circular(24),
+                                ),
+                                color: Colors.white,
+                              ),
+                              padding: EdgeInsets.all(12),
+                              child: Column(
+                                spacing: 16,
+                                children: [
+                                  Expanded(
+                                    child: Container(
+                                      decoration: ShapeDecoration(
+                                        shape: RoundedSuperellipseBorder(
+                                          borderRadius: BorderRadius.only(
+                                            topRight: Radius.circular(24),
+                                            topLeft: Radius.circular(24),
+                                          ),
+                                        ),
+                                        color: Colors.orange,
                                       ),
                                     ),
-                                    color: Colors.orange,
                                   ),
+                                  Text("Home Cleaning")
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "Home Repairs",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: () {},
+                              style: TextButton.styleFrom(
+                                foregroundColor: Colors.purple,
+                                textStyle: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
                                 ),
                               ),
-                              Text("Home Cleaning")
-                            ],
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Home Repairs",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {},
-                          style: TextButton.styleFrom(
-                            foregroundColor: Colors.purple,
-                            textStyle: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
+                              child: Text("See All"),
                             ),
-                          ),
-                          child: Text("See All"),
+                          ],
                         ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    height: 200,
-                    padding: EdgeInsets.only(
-                        left: 16
-                    ),
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (context, index) {
-                        return Container(
-                          margin: EdgeInsets.only(right: 12),
-                          width: 150,
-                          decoration: ShapeDecoration(
-                            shape: RoundedSuperellipseBorder(
-                              borderRadius: BorderRadius.circular(24),
-                            ),
-                            color: Colors.white,
-                          ),
-                          padding: EdgeInsets.all(12),
-                          child: Column(
-                            spacing: 16,
-                            children: [
-                              Expanded(
-                                child: Container(
-                                  decoration: ShapeDecoration(
-                                    shape: RoundedSuperellipseBorder(
-                                      borderRadius: BorderRadius.only(
-                                        topRight: Radius.circular(24),
-                                        topLeft: Radius.circular(24),
+                      ),
+                      Container(
+                        height: 200,
+                        padding: EdgeInsets.only(
+                            left: 16
+                        ),
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              margin: EdgeInsets.only(right: 12),
+                              width: 150,
+                              decoration: ShapeDecoration(
+                                shape: RoundedSuperellipseBorder(
+                                  borderRadius: BorderRadius.circular(24),
+                                ),
+                                color: Colors.white,
+                              ),
+                              padding: EdgeInsets.all(12),
+                              child: Column(
+                                spacing: 16,
+                                children: [
+                                  Expanded(
+                                    child: Container(
+                                      decoration: ShapeDecoration(
+                                        shape: RoundedSuperellipseBorder(
+                                          borderRadius: BorderRadius.only(
+                                            topRight: Radius.circular(24),
+                                            topLeft: Radius.circular(24),
+                                          ),
+                                        ),
+                                        color: Colors.orange,
                                       ),
                                     ),
-                                    color: Colors.orange,
                                   ),
-                                ),
+                                  Text("Home Cleaning")
+                                ],
                               ),
-                              Text("Home Cleaning")
-                            ],
-                          ),
-                        );
-                      },
-                    ),
-                  ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),)),
+
                 ],
               ),
             ),
