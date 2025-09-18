69aac34ed10952e470bb714c40c8b75d8e822974
JAICHANGPARK <aristojeff@gmail.com>
2025-09-18 18:46:35 +0900

    Update home_cleaning_detail_page.dart

--- lib/ep2368_home_cleaning_app/ui/home_cleaning_detail_page.dart
+++ lib/ep2368_home_cleaning_app/ui/home_cleaning_detail_page.dart
@@ -18,9 +18,21 @@ class _HomeCleaningDetailPageState extends State<HomeCleaningDetailPage> {
           Expanded(
             child: Column(
               children: [
-                Row(children: [Text("Fix my house"), Text("\$60.00"),
-                Text("")
-                ]),
+                Row(
+                  children: [
+                    Text("Fix my house"),
+                    Text("\$60.00"),
+
+                  ],
+                ),
+                Text(
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                  maxLines: 3,
+                ),
+                Container(
+                  height: 200,
+                  child: Placeholder(),
+                )
               ],
             ),
           ),
