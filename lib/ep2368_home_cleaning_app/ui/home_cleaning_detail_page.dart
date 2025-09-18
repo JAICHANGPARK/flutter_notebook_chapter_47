e6d448c4dc9e3d138b2eafffdce291c3b0bcb8b6
JAICHANGPARK <aristojeff@gmail.com>
2025-09-18 18:42:52 +0900

    Update home_cleaning_detail_page.dart

--- lib/ep2368_home_cleaning_app/ui/home_cleaning_detail_page.dart
+++ lib/ep2368_home_cleaning_app/ui/home_cleaning_detail_page.dart
@@ -1,1 +1,23 @@
 import 'package:flutter/material.dart';
+
+class HomeCleaningDetailPage extends StatefulWidget {
+  const HomeCleaningDetailPage({super.key});
+
+  @override
+  State<HomeCleaningDetailPage> createState() => _HomeCleaningDetailPageState();
+}
+
+class _HomeCleaningDetailPageState extends State<HomeCleaningDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: 250,
+          )
+        ],
+      ),
+    );
+  }
+}
