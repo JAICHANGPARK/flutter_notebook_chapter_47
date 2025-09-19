ccf4436216b0a52217cfb0c0e1ea0222452f66f4
JAICHANGPARK <aristojeff@gmail.com>
2025-09-19 17:10:36 +0900

    Update home_cleaning_detail_page.dart

--- lib/ep2368_home_cleaning_app/ui/home_cleaning_detail_page.dart
+++ lib/ep2368_home_cleaning_app/ui/home_cleaning_detail_page.dart
@@ -73,13 +73,23 @@ class _HomeCleaningDetailPageState extends State<HomeCleaningDetailPage> {
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                      Text("\$60.00"),
+                      Text(
+                        "\$60.00",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.purple,
+                          fontSize: 20,
+                        ),
+                      ),
                     ],
                   ),
                 ),
-                Text(
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                  maxLines: 3,
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                    maxLines: 3,
+                  ),
                 ),
                 Container(height: 200, child: Placeholder()),
                 Text("Tags"),
