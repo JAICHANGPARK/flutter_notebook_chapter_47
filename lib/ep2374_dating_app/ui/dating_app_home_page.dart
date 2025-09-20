2bbe6197fb7e38586dd02e8de3155ba97654e290
JAICHANGPARK <aristojeff@gmail.com>
2025-09-20 09:04:44 +0900

    Create dating_app_home_page.dart

---
+++ lib/ep2374_dating_app/ui/dating_app_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class DatingAppHomePage extends StatefulWidget {
+  const DatingAppHomePage({super.key});
+
+  @override
+  State<DatingAppHomePage> createState() => _DatingAppHomePageState();
+}
+
+class _DatingAppHomePageState extends State<DatingAppHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
