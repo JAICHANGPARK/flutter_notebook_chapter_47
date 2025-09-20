a85424d0293b187a5d7691bd299547623cd6e98a
JAICHANGPARK <aristojeff@gmail.com>
2025-09-20 09:05:38 +0900

    Update dating_app_home_page.dart

--- lib/ep2374_dating_app/ui/dating_app_home_page.dart
+++ lib/ep2374_dating_app/ui/dating_app_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class DatingAppHomePage extends StatefulWidget {
   const DatingAppHomePage({super.key});
 
@@ -11,6 +10,26 @@ class DatingAppHomePage extends StatefulWidget {
 class _DatingAppHomePageState extends State<DatingAppHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                CircleAvatar(),
+                Expanded(child: Column(children: [])),
+                Container(decoration: BoxDecoration(shape: BoxShape.circle)),
+              ],
+            ),
+            Container(
+              decoration: ShapeDecoration(
+                shape: StadiumBorder(),
+                color: Colors.grey[200]!,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
