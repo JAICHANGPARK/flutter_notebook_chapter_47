dda0206502ab77ba4fe7cac858d2550dce5c0129
JAICHANGPARK <aristojeff@gmail.com>
2025-09-20 09:03:23 +0900

    Create dating_app.dart

---
+++ lib/ep2374_dating_app/dating_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class DatingApp extends StatefulWidget {
+  const DatingApp({super.key});
+
+  @override
+  State<DatingApp> createState() => _DatingAppState();
+}
+
+class _DatingAppState extends State<DatingApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
