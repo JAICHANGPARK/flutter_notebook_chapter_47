747708e1367c5aff2078377ac583637469e2ae07
JAICHANGPARK <aristojeff@gmail.com>
2025-09-22 17:38:32 +0900

    Update dating_app.dart

--- lib/ep2374_dating_app/dating_app.dart
+++ lib/ep2374_dating_app/dating_app.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'ui/dating_app_home_page.dart';
-
 class DatingApp extends StatelessWidget {
   const DatingApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(home: DatingAppHomePage());
