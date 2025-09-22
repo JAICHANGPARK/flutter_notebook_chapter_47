4f3276a6335a89d813068e2cc72b3a2c2e9e8bd8
JAICHANGPARK <aristojeff@gmail.com>
2025-09-22 17:38:25 +0900

    Update dating_app.dart

--- lib/ep2374_dating_app/dating_app.dart
+++ lib/ep2374_dating_app/dating_app.dart
@@ -4,11 +4,8 @@ import 'ui/dating_app_home_page.dart';
 
 class DatingApp extends StatelessWidget {
   const DatingApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DatingAppHomePage(),
-    );
+    return MaterialApp(home: DatingAppHomePage());
   }
 }
