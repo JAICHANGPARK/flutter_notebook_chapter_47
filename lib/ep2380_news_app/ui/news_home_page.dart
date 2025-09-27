005051a3adc3279e64c030265aa0ce59b7850aff
JAICHANGPARK <aristojeff@gmail.com>
2025-09-27 10:27:11 +0900

    Update news_home_page.dart

---
+++ lib/ep2380_news_app/ui/news_home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class NewsHomePage extends StatefulWidget {
+  const NewsHomePage({super.key});
+
+  @override
+  State<NewsHomePage> createState() => _NewsHomePageState();
+}
+
+class _NewsHomePageState extends State<NewsHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
