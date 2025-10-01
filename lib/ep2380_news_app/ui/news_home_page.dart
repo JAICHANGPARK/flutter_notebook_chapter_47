c06c61a645c32db02b2dc0287b57ee01593e80d6
JAICHANGPARK <aristojeff@gmail.com>
2025-10-01 18:58:04 +0900

    Update news_home_page.dart

--- lib/ep2380_news_app/ui/news_home_page.dart
+++ lib/ep2380_news_app/ui/news_home_page.dart
@@ -202,48 +202,53 @@ class _NewsHomePageState extends State<NewsHomePage> {
                       scrollDirection: Axis.horizontal,
                       itemCount: 10,
                       itemBuilder: (context, index) {
-                        return Container(
-                          width: MediaQuery.sizeOf(context).width * .85,
-                          margin: EdgeInsets.only(left: 16),
-                          decoration: BoxDecoration(
-                            color: Colors.blue,
-                            borderRadius: BorderRadius.circular(16),
-                          ),
-                          padding: EdgeInsets.all(8),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            spacing: 6,
-                            children: [
-                              Row(
-                                spacing: 6,
-                                children: [
-                                  CircleAvatar(radius: 12),
-                                  Text(
-                                    "ABCD 10h ago",
-                                    style: TextStyle(
-                                      fontSize: 12,
-                                      color: Colors.grey,
+                        return GestureDetector(
+                          onTap: (){
+                            
+                          },
+                          child: Container(
+                            width: MediaQuery.sizeOf(context).width * .85,
+                            margin: EdgeInsets.only(left: 16),
+                            decoration: BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                            padding: EdgeInsets.all(8),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              spacing: 6,
+                              children: [
+                                Row(
+                                  spacing: 6,
+                                  children: [
+                                    CircleAvatar(radius: 12),
+                                    Text(
+                                      "ABCD 10h ago",
+                                      style: TextStyle(
+                                        fontSize: 12,
+                                        color: Colors.grey,
+                                      ),
                                     ),
+                                  ],
+                                ),
+                                Text(
+                                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+                                  maxLines: 2,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 12,
                                   ),
-                                ],
-                              ),
-                              Text(
-                                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
-                                maxLines: 2,
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 12,
                                 ),
-                              ),
-                              Text(
-                                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
-                                maxLines: 1,
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 12,
+                                Text(
+                                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+                                  maxLines: 1,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 12,
+                                  ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         );
                       },
