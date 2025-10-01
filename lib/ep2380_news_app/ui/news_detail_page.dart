ba5449cc805916719b8b1c250e8d3d997ba69df3
JAICHANGPARK <aristojeff@gmail.com>
2025-10-01 19:08:34 +0900

    Update news_detail_page.dart

--- lib/ep2380_news_app/ui/news_detail_page.dart
+++ lib/ep2380_news_app/ui/news_detail_page.dart
@@ -151,10 +151,21 @@ class _NewsDetailPageState extends State<NewsDetailPage> {
                       style: TextStyle(fontSize: 10, color: Colors.grey),
                     ),
                   ),
-                  Column(
-                    children: [
-                      
-                    ],
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Column(
+                      spacing: 20,
+                      children: [
+                        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+                        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+                        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+                        Container(
+                          height: 240,
+                          margin: EdgeInsets.symmetric(horizontal: 16,),
+                          decoration: BoxDecoration(color: Colors.black),
+                        ),
+                      ],
+                    ),
                   )
 
                 ],
