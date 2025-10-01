1a4a1d31af40ec5731b1d70811d215f53fb34bc2
JAICHANGPARK <aristojeff@gmail.com>
2025-10-02 08:02:37 +0900

    Update news_detail_page.dart

--- lib/ep2380_news_app/ui/news_detail_page.dart
+++ lib/ep2380_news_app/ui/news_detail_page.dart
@@ -194,7 +194,9 @@ class _NewsDetailPageState extends State<NewsDetailPage> {
           ),
           Align(
             child: Container(
-              decoration: ShapeDecoration(shape: StadiumBorder(),
+              decoration: ShapeDecoration(
+                shape: StadiumBorder(),
+                color: Colors.white,
               ),
             ),
           ),
