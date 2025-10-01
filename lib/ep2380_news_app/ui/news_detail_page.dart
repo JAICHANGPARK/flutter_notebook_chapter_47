0c4737827299641307d853e8e94d1886ee877030
JAICHANGPARK <aristojeff@gmail.com>
2025-10-01 19:06:44 +0900

    Update news_detail_page.dart

--- lib/ep2380_news_app/ui/news_detail_page.dart
+++ lib/ep2380_news_app/ui/news_detail_page.dart
@@ -33,131 +33,127 @@ class _NewsDetailPageState extends State<NewsDetailPage> {
               child: Column(
                 spacing: 12,
                 children: [
-              Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 8,
-              ),
-              child: Row(
-                spacing: 8,
-                children: [
-                  InkWell(
-                    onTap: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: Icon(Icons.keyboard_arrow_left),
-                  ),
-                  Text(
-                    "Back",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
-                  ),
-                  Spacer(),
-                  Container(
-                    decoration: ShapeDecoration(
-                      shape: StadiumBorder(),
-                      color: Colors.white,
-                    ),
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 8,
-                      vertical: 6,
-                    ),
-                    child: Row(children: [Text("⛈ 50℉")]),
-                  ),
-                  CircleAvatar(
-                    backgroundColor: Colors.white,
-                    foregroundColor: Colors.black,
-                    child: Badge(child: Icon(Icons.bookmark_border)),
-                  ),
-                  CircleAvatar(
-                    backgroundColor: Colors.white,
-                    foregroundColor: Colors.black,
-                    child: Icon(Icons.share),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Row(
-                spacing: 8,
-                children: [
-                  CircleAvatar(),
-                  Text("ABCD KOREA · Sep 30 · Politics"),
-                  Spacer(),
-                  Container(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 24,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
                       vertical: 8,
                     ),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(4),
-                      gradient: LinearGradient(
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                        colors: [
-                          Color.fromRGBO(49, 102, 223, 1),
-                          Color.fromRGBO(67, 118, 239, 1),
-                          Color.fromRGBO(49, 102, 223, 1),
+                    child: Row(
+                      spacing: 8,
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Icon(Icons.keyboard_arrow_left),
+                        ),
+                        Text(
+                          "Back",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        Spacer(),
+                        Container(
+                          decoration: ShapeDecoration(
+                            shape: StadiumBorder(),
+                            color: Colors.white,
+                          ),
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 8,
+                            vertical: 6,
+                          ),
+                          child: Row(children: [Text("⛈ 50℉")]),
+                        ),
+                        CircleAvatar(
+                          backgroundColor: Colors.white,
+                          foregroundColor: Colors.black,
+                          child: Badge(child: Icon(Icons.bookmark_border)),
+                        ),
+                        CircleAvatar(
+                          backgroundColor: Colors.white,
+                          foregroundColor: Colors.black,
+                          child: Icon(Icons.share),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Row(
+                      spacing: 8,
+                      children: [
+                        CircleAvatar(),
+                        Text("ABCD KOREA · Sep 30 · Politics"),
+                        Spacer(),
+                        Container(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 24,
+                            vertical: 8,
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(4),
+                            gradient: LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                              colors: [
+                                Color.fromRGBO(49, 102, 223, 1),
+                                Color.fromRGBO(67, 118, 239, 1),
+                                Color.fromRGBO(49, 102, 223, 1),
+                              ],
+                            ),
+                          ),
+                          child: Text(
+                            "Follow",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: IntrinsicHeight(
+                      child: Row(
+                        spacing: 12,
+                        children: [
+                          SizedBox(
+                            height: 32,
+
+                            // width: 6,
+                            child: VerticalDivider(
+                              width: 2,
+                              color: Colors.blue,
+                              thickness: 4,
+                            ),
+                          ),
+                          Expanded(
+                            child: Text(
+                              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, ",
+                              maxLines: 1,
+                            ),
+                          ),
                         ],
                       ),
                     ),
-                    child: Text(
-                      "Follow",
-                      style: TextStyle(color: Colors.white),
-                    ),
+                  ),
+                  Container(
+                    height: 240,
+                    margin: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                    decoration: BoxDecoration(color: Colors.black),
+                  ),
+                  Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 10, color: Colors.grey),
                   ),
                 ],
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: IntrinsicHeight(
-                child: Row(
-                  spacing: 12,
-                  children: [
-                    SizedBox(
-                      height: 32,
-
-                      // width: 6,
-                      child: VerticalDivider(
-                        width: 2,
-                        color: Colors.blue,
-                        thickness: 4,
-                      ),
-                    ),
-                    Expanded(
-                      child: Text(
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, ",
-                        maxLines: 1,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Container(
-              height: 240,
-              margin: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-              decoration: BoxDecoration(
-                color: Colors.black,
-              ),
-            ),
-            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-
-            ),)
+          ),
         ],
       ),
-    ),)
-    ,
-    ]
-    ,
-    )
-    ,
     );
   }
 }
