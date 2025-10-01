3ecc66879f43c01786c71ab6a56a37f19b074899
JAICHANGPARK <aristojeff@gmail.com>
2025-10-01 18:57:06 +0900

    Update news_detail_page.dart

--- lib/ep2380_news_app/ui/news_detail_page.dart
+++ lib/ep2380_news_app/ui/news_detail_page.dart
@@ -14,7 +14,67 @@ class _NewsDetailPageState extends State<NewsDetailPage> {
     return Scaffold(
       body: Stack(
         children: [
-          
+          Positioned.fill(
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color.fromRGBO(239, 243, 255, 1),
+                    Colors.white,
+                    Color.fromRGBO(243, 246, 254, 1),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned.fill(
+            child: SafeArea(
+              child: Column(
+                spacing: 12,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 8,
+                    ),
+                    child: Row(
+                      spacing: 8,
+                      children: [
+                        Text(
+                          "Explore News",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        Spacer(),
+                        Container(
+                          decoration: ShapeDecoration(
+                            shape: StadiumBorder(),
+                            color: Colors.white,
+                          ),
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 8,
+                            vertical: 6,
+                          ),
+                          child: Row(children: [Text("⛈ 50℉")]),
+                        ),
+                        CircleAvatar(
+                          backgroundColor: Colors.white,
+                          child: Badge(
+                            child: Icon(Icons.notifications_active_outlined),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
