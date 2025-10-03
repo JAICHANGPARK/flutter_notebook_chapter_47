90d1ba5725a20d610a1cfcdefb0fcfc9c0ef8d4e
JAICHANGPARK <aristojeff@gmail.com>
2025-10-03 09:53:11 +0900

    Update employee_app.dart

--- lib/ep2386_employee_app/employee_app.dart
+++ lib/ep2386_employee_app/employee_app.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'ui/employee_detail_page.dart';
+
 class EmployeeApp extends StatelessWidget {
   const EmployeeApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      home: EmployeeDetailPage(),
     );
   }
 }
