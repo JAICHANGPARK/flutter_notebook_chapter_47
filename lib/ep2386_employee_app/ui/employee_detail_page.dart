ce595a0415cb01f8c3a11d1c5c7160d3cf023eb0
JAICHANGPARK <aristojeff@gmail.com>
2025-10-03 09:52:37 +0900

    Update employee_detail_page.dart

--- lib/ep2386_employee_app/ui/employee_detail_page.dart
+++ lib/ep2386_employee_app/ui/employee_detail_page.dart
@@ -11,6 +11,8 @@ class EmployeeDetailPage extends StatefulWidget {
 class _EmployeeDetailPageState extends State<EmployeeDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      
+    );
   }
 }
