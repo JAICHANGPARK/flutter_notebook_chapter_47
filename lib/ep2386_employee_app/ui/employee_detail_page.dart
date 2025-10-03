f18d39ce73fbf711b7ce1c34cc87aa85a4281847
JAICHANGPARK <aristojeff@gmail.com>
2025-10-03 09:55:30 +0900

    Update employee_detail_page.dart

--- lib/ep2386_employee_app/ui/employee_detail_page.dart
+++ lib/ep2386_employee_app/ui/employee_detail_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class EmployeeDetailPage extends StatefulWidget {
   const EmployeeDetailPage({super.key});
 
@@ -12,7 +11,12 @@ class _EmployeeDetailPageState extends State<EmployeeDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Employee details"),
+        foregroundColor: Colors.white,
+        backgroundColor: Color.fromRGBO(28, 33, 45, 1),
+      ),
     );
   }
 }
