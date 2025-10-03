080831aee82fd7ea045fc37268079740282ea864
JAICHANGPARK <aristojeff@gmail.com>
2025-10-03 09:52:29 +0900

    Create employee_detail_page.dart

---
+++ lib/ep2386_employee_app/ui/employee_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class EmployeeDetailPage extends StatefulWidget {
+  const EmployeeDetailPage({super.key});
+
+  @override
+  State<EmployeeDetailPage> createState() => _EmployeeDetailPageState();
+}
+
+class _EmployeeDetailPageState extends State<EmployeeDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
