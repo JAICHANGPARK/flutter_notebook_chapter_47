35707f8b7ee56f6641f37689742777a98f786506
JAICHANGPARK <aristojeff@gmail.com>
2025-10-04 08:57:00 +0900

    Update employee_personal_page.dart

---
+++ lib/ep2386_employee_app/ui/widgets/employee_personal_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class EmployeePersonalPage extends StatefulWidget {
+  const EmployeePersonalPage({super.key});
+
+  @override
+  State<EmployeePersonalPage> createState() => _EmployeePersonalPageState();
+}
+
+class _EmployeePersonalPageState extends State<EmployeePersonalPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
