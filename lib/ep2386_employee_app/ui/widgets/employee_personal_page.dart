5d213b0efe8b525837537d50e09ea0d3923dc76b
JAICHANGPARK <aristojeff@gmail.com>
2025-10-04 09:02:14 +0900

    Update employee_personal_page.dart

--- lib/ep2386_employee_app/ui/widgets/employee_personal_page.dart
+++ lib/ep2386_employee_app/ui/widgets/employee_personal_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class EmployeePersonalPage extends StatefulWidget {
   const EmployeePersonalPage({super.key});
 
@@ -19,18 +18,22 @@ class _EmployeePersonalPageState extends State<EmployeePersonalPage> {
           children: [
             Icon(Icons.phone_android),
             Text("Phone number"),
-            Container(decoration: ShapeDecoration(shape: StadiumBorder()),
-            child: Text("+00-123-456-789"),)
+            Container(
+              decoration: ShapeDecoration(shape: StadiumBorder()),
+              child: Text("+00-123-456-789"),
+            ),
           ],
         ),
         Row(
           children: [
-            Icon(Icons.email_outlined),
+            Icon(Icons.email_outlined, size: 16),
             Text("Email"),
-            Container(decoration: ShapeDecoration(shape: StadiumBorder()),
-              child: Text("example@sample.com"),)
+            Container(
+              decoration: ShapeDecoration(shape: StadiumBorder()),
+              child: Text("example@sample.com"),
+            ),
           ],
-        )
+        ),
       ],
     );
   }
