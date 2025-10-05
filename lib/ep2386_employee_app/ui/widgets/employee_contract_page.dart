2d256d7d9272574e18e92776df51eb9c1cee1925
JAICHANGPARK <aristojeff@gmail.com>
2025-10-05 11:34:39 +0900

    Create employee_contract_page.dart

---
+++ lib/ep2386_employee_app/ui/widgets/employee_contract_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class EmployeeContractPage extends StatefulWidget {
+  const EmployeeContractPage({super.key});
+
+  @override
+  State<EmployeeContractPage> createState() => _EmployeeContractPageState();
+}
+
+class _EmployeeContractPageState extends State<EmployeeContractPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [],
+    );
+  }
+}
