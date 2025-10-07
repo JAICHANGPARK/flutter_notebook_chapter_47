790327f77df0d7357db6c43389238661a2203996
JAICHANGPARK <aristojeff@gmail.com>
2025-10-07 10:33:38 +0900

    Update employee_payroll_page.dart

---
+++ lib/ep2386_employee_app/ui/widgets/employee_payroll_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class EmployeePayrollPage extends StatefulWidget {
+  const EmployeePayrollPage({super.key});
+
+  @override
+  State<EmployeePayrollPage> createState() => _EmployeePayrollPageState();
+}
+
+class _EmployeePayrollPageState extends State<EmployeePayrollPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
