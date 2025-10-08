ecbefcc234508c446919de997fb496ee0fbd0afe
JAICHANGPARK <aristojeff@gmail.com>
2025-10-08 10:48:12 +0900

    Update doctor_appointment_home_page.dart

--- lib/ep2391_doctor_appointment_app/ui/doctor_appointment_home_page.dart
+++ lib/ep2391_doctor_appointment_app/ui/doctor_appointment_home_page.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/material.dart';
 
-
 class DoctorAppointmentHomePage extends StatefulWidget {
   const DoctorAppointmentHomePage({super.key});
 
   @override
-  State<DoctorAppointmentHomePage> createState() => _DoctorAppointmentHomePageState();
+  State<DoctorAppointmentHomePage> createState() =>
+      _DoctorAppointmentHomePageState();
 }
 
 class _DoctorAppointmentHomePageState extends State<DoctorAppointmentHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      body: SafeArea(
-        child: Column(
+      body: SafeArea(child: Column(children: [
+        Row(
           children: [
-            
+            Text("Doctor List"),
+            CircleAvatar(backgroundColor: Colors.white,)
           ],
-        ),
-      ),
+        )
+      ],)),
     );
   }
 }
