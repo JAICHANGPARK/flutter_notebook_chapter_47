70b4f424336ea3c484be27ab960fa6f5e3abaacb
JAICHANGPARK <aristojeff@gmail.com>
2025-10-08 10:45:07 +0900

    Update doctor_appointment_app.dart

--- lib/ep2391_doctor_appointment_app/doctor_appointment_app.dart
+++ lib/ep2391_doctor_appointment_app/doctor_appointment_app.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 
-
-
 class DoctorAppointmentApp extends StatelessWidget {
   const DoctorAppointmentApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      
-    );
+    return MaterialApp();
   }
 }
