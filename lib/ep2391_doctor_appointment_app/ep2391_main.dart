e272495ed4eaf497bc4d85cf7147e01ed9d27f38
JAICHANGPARK <aristojeff@gmail.com>
2025-10-08 10:46:25 +0900

    Update ep2391_main.dart

--- lib/ep2391_doctor_appointment_app/ep2391_main.dart
+++ lib/ep2391_doctor_appointment_app/ep2391_main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_47/ep2391_doctor_appointment_app/doctor_appointment_app.dart';
 
 void main() {
-
+    runApp(DoctorAppointmentApp());
 }
