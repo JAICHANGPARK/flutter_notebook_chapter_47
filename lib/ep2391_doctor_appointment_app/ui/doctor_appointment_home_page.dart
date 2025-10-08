20f480ef415de2035f37649fc5cade3f2563b5c5
JAICHANGPARK <aristojeff@gmail.com>
2025-10-08 10:45:26 +0900

    Create doctor_appointment_home_page.dart

---
+++ lib/ep2391_doctor_appointment_app/ui/doctor_appointment_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class DoctorAppointmentHomePage extends StatefulWidget {
+  const DoctorAppointmentHomePage({super.key});
+
+  @override
+  State<DoctorAppointmentHomePage> createState() => _DoctorAppointmentHomePageState();
+}
+
+class _DoctorAppointmentHomePageState extends State<DoctorAppointmentHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
