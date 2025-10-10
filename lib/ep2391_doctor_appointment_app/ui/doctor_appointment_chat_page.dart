878b342badee8b875852125c5611f1480e412493
JAICHANGPARK <aristojeff@gmail.com>
2025-10-10 18:57:07 +0900

    Update doctor_appointment_chat_page.dart

--- lib/ep2391_doctor_appointment_app/ui/doctor_appointment_chat_page.dart
+++ lib/ep2391_doctor_appointment_app/ui/doctor_appointment_chat_page.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
 
+class DoctorAppointmentChatPage extends StatefulWidget {
+  const DoctorAppointmentChatPage({super.key});
+
+  @override
+  State<DoctorAppointmentChatPage> createState() => _DoctorAppointmentChatPageState();
+}
+
+class _DoctorAppointmentChatPageState extends State<DoctorAppointmentChatPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+
+    );
+  }
+}
