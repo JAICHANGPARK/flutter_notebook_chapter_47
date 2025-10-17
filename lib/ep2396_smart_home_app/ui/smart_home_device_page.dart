6deb1319e42c42c6893c78fce312c233c930f7e0
JAICHANGPARK <aristojeff@gmail.com>
2025-10-17 18:26:28 +0900

    Update smart_home_device_page.dart

--- lib/ep2396_smart_home_app/ui/smart_home_device_page.dart
+++ lib/ep2396_smart_home_app/ui/smart_home_device_page.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
 
+
+class SmartHomeDevicePage extends StatefulWidget {
+  const SmartHomeDevicePage({super.key});
+
+  @override
+  State<SmartHomeDevicePage> createState() => _SmartHomeDevicePageState();
+}
+
+class _SmartHomeDevicePageState extends State<SmartHomeDevicePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
