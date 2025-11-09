95000a4f2bb3d5a4feefea5a638cff699bc7539f
Dreamwalker <aristojeff@gmail.com>
2025-11-09 10:00:00 +0900

    change

--- lib/ep2351_furniture_store_app/furniture_store_app.dart
+++ lib/ep2351_furniture_store_app/furniture_store_app.dart
@@ -2,14 +2,11 @@ import 'package:flutter/material.dart';
 
 import 'ui/furniture_store_home_page.dart';
 
-
 class FurnitureStoreApp extends StatelessWidget {
   const FurnitureStoreApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: FurnitureStoreHomePage(),
-    );
+    return MaterialApp(home: FurnitureStoreHomePage());
   }
 }
